ca58c2e2068080c98e92dc80dc80d9053dadd3bf
Nugie kurniawan <nugiebinhar@gmail.com>
2025-07-18 00:21:31 +0700

    Mengatur seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Sekolah;
 use Illuminate\Database\Seeder;
+use Database\Seeders\JurusanSeeder;
+use Illuminate\Support\Facades\Http;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([JurusanSeeder::class]);
+            $response = Http::get("https://api-sekolah-indonesia.vercel.app/sekolah/SMK?page=1&perPage=1000");
+    $data = $response->json();
+    foreach ($data["dataSekolah"] as $item) {
+        Sekolah::create([
+            "nama_sekolah" => $item["sekolah"],
+            "npsn" => $item["npsn"],
+            "provinsi" => $item["propinsi"],
+            "alamat" => $item["kabupaten_kota"]." ".$item["kecamatan"]." ".$item["alamat_jalan"]
+        ]);
+    }
     }
 }
